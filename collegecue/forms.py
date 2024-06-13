9bf23730b4244653de2736ceb1b07b60398305f7
asparsh607 <113459577+asparsh607@users.noreply.github.com>
2024-06-13 21:26:52 +0530

    added updated forms.py

---
+++ collegecue/forms.py
@@ -0,0 +1,63 @@
+from django import forms # type: ignore
+from .models import *
+
+class CompanyInChargeForm(forms.ModelForm):
+    class Meta:
+        model = CompanyInCharge
+        fields = '__all__'
+
+class UniversityInChargeForm(forms.ModelForm):
+    class Meta:
+        model = UniversityInCharge
+        fields = '__all__'
+
+class ConsultantForm(forms.ModelForm):
+    class Meta:
+        model = Consultant
+        fields = '__all__'
+
+class SubscriptionForm(forms.ModelForm):
+    class Meta:
+        model = Subscriber
+        fields = ['email']
+        widgets = {
+            'email': forms.EmailInput(attrs={'placeholder': 'Enter your email'})
+}
+
+class ForgotForm(forms.ModelForm):
+    class Meta:
+        model = Forgot
+        fields = ['email']
+
+class VerifyForm(forms.ModelForm):
+    class Meta:
+        model = Verify
+        fields = ['otp']
+
+class Forgot2Form(forms.ModelForm):
+    class Meta:
+        model = Forgot2
+        fields = '__all__'
+
+class RegisterForm(forms.ModelForm):
+    class Meta:
+        model = new_user
+        fields = ['firstname','lastname','phonenumber','country_code','email','password']
+
+class NextForm(forms.ModelForm):
+    class Meta:
+        model = new_user
+        fields = ['course','education','percentage','preferred_destination','start_date','mode_study','entrance','passport','country_code','phonenumber']
+
+class LoginForm(forms.ModelForm):
+    class Meta:
+        model = new_user
+        fields =['email','password']
+
+class SubscriptionForm1(forms.ModelForm):
+    class Meta:
+        model = Subscriber1
+        fields = ['email']
+        widgets = {
+            'email': forms.EmailInput(attrs={'placeholder': 'Enter your email'})
+}
